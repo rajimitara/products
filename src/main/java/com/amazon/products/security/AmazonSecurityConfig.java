29f8be6cff5cc157a83b45027e41392831053209
rajimitara <rajimitara@gmail.com>
2020-02-20 18:18:08 +0530

    Build & Tested

--- src/main/java/com/amazon/products/security/AmazonSecurityConfig.java
+++ src/main/java/com/amazon/products/security/AmazonSecurityConfig.java
@@ -3,12 +3,15 @@ package com.amazon.products.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
@@ -18,23 +21,26 @@ public class AmazonSecurityConfig extends WebSecurityConfigurerAdapter {
     //authorization
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http.authorizeRequests();
                 .antMatchers("/").permitAll()
-                .antMatchers("/api/products").hasRole("USER")
-                .antMatchers("/api/products/update").hasRole("USER"); //TODO:add SELLER role for update
+                //.antMatchers("/api/products").hasAnyRole("GUEST","SELLER")
+                //.antMatchers("/api/products/update").hasRole("SELLER");
+                // //TODO:add SELLER role for update
     }
     //InMemory user details, authentication
-    @Bean
+    //authorization
     @Override
-    public UserDetailsService userDetailsService() {
-        UserDetails user =
-                User.withDefaultPasswordEncoder()
-                        .username("id")
-                        .password("password")
-                        .roles("USER")
-                        .build();
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .passwordEncoder(passwordEncoder())
+                .withUser("id")
+                .password(passwordEncoder().encode("password"))
+                .authorities("GUEST","SELLER");
+    }
 
-        return new InMemoryUserDetailsManager(user);
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
